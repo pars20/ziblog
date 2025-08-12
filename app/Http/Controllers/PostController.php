ed77fa6c0d2b620a6ddce53ce94615590e257079
pars20 <pars20@yahoo.com>
2025-08-13 03:20:34 +0330

    upload post cover optimized

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -36,12 +37,13 @@ class PostController extends Controller
      */
     public function store(CreatePostRequest $request)
     {
-        $data = $request->validated();
-        if( $request->hasFile('image') ){
-            $data['image'] = $request->file('image')->store('post_images','public');
+        $post = $request->user()->posts()->create( $request->validated() );
+        $postCover = $this->savePostCover( $request, $post );
+        if( $postCover ){
+            $post->image = $postCover;
+            $post->save();
         }
-        $post = $request->user()->posts()->create( $data );
-        // $post = Post::create( $request->validated() );
+
         return redirect()
             ->route( 'posts.show', $post )
             ->with('success','این پست همین الان منتشر شد!');
@@ -70,12 +72,11 @@ class PostController extends Controller
      */
     public function update(UpdatePostRequest $request, Post $post)
     {
-        // Gate::authorize( 'update', $post );
         $data = $request->validated();
-        if( $request->hasFile('image') ){
-            $data['image'] = $request->file('image')->store('post_images','public');
-        }
+        $postCover = $this->savePostCover( $request, $post );
+        if( $postCover ) $data['image'] = $postCover;
         $post->update( $data );
+
         return redirect()->route( 'posts.show', $post )
             ->with( 'success', 'این پست همین الان آپدیت شد!' );
     }
@@ -86,9 +87,42 @@ class PostController extends Controller
     public function destroy(Post $post)
     {
         Gate::authorize( 'delete', $post );
+
+        if( $post->image ){
+            Storage::disk('public')->delete( $post->image );
+        }
+
         $post->delete();
+
         return redirect()
             ->route( 'posts.index' )
             ->with('success','پست حذف شد!');
     }
+
+
+    private function savePostCover( $request, $post ) : ? string
+    {
+        if( !$request->hasFile('image') ) return null;
+
+        // If an old image exists for this post, delete it first.
+        if ($post->image) {
+            Storage::disk('public')->delete( $post->image );
+        }
+        
+        $file = $request->file('image');
+        $extension = $file->getClientOriginalExtension();
+        $filename = $post->slug.".$extension";
+        $path = "post-images";
+        $counter = 1;
+        while( Storage::disk('public')->exists("$path/$filename") ){
+            $filename = $post->slug ."-$counter.$extension";
+            $counter++;
+        }
+        $imagePath = $file->storeAs($path,$filename,'public');
+        //$post->image = $imagePath;
+        //$post->save();
+        return $imagePath;
+        
+    }
+    
 }
